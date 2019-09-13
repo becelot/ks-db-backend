33a40ea593bbc4b547e941c0a40d16529a3cd211
becelot <benedict.becker93@gmail.com>
2019-09-13 22:04:16 +0200

    Added very basic create document function

---
+++ src/functions/create.ts
@@ -0,0 +1,19 @@
+import {DataMapper} from "@aws/dynamodb-data-mapper";
+import {Mapper} from "../utils/mapper";
+import {Document} from "../models/document";
+import {DocType} from "../models/document.type";
+
+export const create = async (event: any, context: any) => {
+    const mapper: DataMapper = Mapper.Instance;
+    const document: Document = new Document();
+    document.name = 'TestFolder';
+    document.children = [];
+    document.type = DocType.DOC_FOLDER;
+
+    await mapper.put(document);
+
+    return {
+        statusCode: '200',
+        message: 'Created folder'
+    }
+};
