3889772d8fe60a32d98c40fc07ce06c753bf3663
becelot <benedict.becker93@gmail.com>
2019-09-14 22:18:48 +0200

    Error handling in CreateDocument

--- src/functions/create.ts
+++ src/functions/create.ts
@@ -2,20 +2,121 @@ import {DataMapper} from "@aws/dynamodb-data-mapper";
 import {Mapper} from "../utils/mapper";
 import {Document} from "../models/document";
 import {DocType} from "../models/document.type";
-import {APIGatewayEvent, APIGatewayProxyResult} from "aws-lambda";
-import {LambdaFunction} from "../utils/aws";
+import {AwsLambda, RestApiEvent, RestApiOutput, toLambda} from "../utils/lambda";
 
-export const create: LambdaFunction = async (event: APIGatewayEvent, context: any): Promise<APIGatewayProxyResult> => {
-    const mapper: DataMapper = Mapper.Instance;
-    const document: Document = new Document();
-    document.name = 'TestFolder';
-    document.children = [];
-    document.type = DocType.DOC_FOLDER;
+interface ICreateDocument {
+    documentName: string;
+    parentFolder: string;
+    date: number;
+    type: DocType;
+}
 
-    const answer = await mapper.put(document);
+interface ICreateDocumentResp {
+    sucessfull: boolean;
+    reason?: string;
+}
 
-    return {
-        statusCode: 200,
-        body: JSON.stringify(answer)
+/**
+ * Try to create a new document in the database
+ */
+class CreateDocument extends AwsLambda<ICreateDocument, ICreateDocumentResp> {
+    public async perform(input: RestApiEvent<ICreateDocument>): Promise<RestApiOutput<ICreateDocumentResp>> {
+        const mapper: DataMapper = Mapper.Instance;
+
+        const query: Document = new Document();
+
+        if (!input.body) {
+            return this.responseBuilder(500, {
+                sucessfull: false,
+                reason: 'No input parameters were provided'
+            });
+        }
+
+        if (!input.body.documentName) {
+            return this.responseBuilder(500, {
+                sucessfull: false,
+                reason: 'Document name was not provided!'
+            });
+        }
+
+        if (input.body.type === undefined) {
+            return this.responseBuilder(500, {
+                sucessfull: false,
+                reason: 'Invalid document type'
+            });
+        }
+
+        query.name = input.body.parentFolder;
+        let parentFolder: Document | undefined = undefined;
+
+        // If the file is created in another folder
+        if (!!input.body.parentFolder) {
+            // Check if the parent exists and is a folder
+            try {
+                parentFolder = await mapper.get<Document>(query);
+                if (parentFolder.type === DocType.DOC_FILE) {
+                    return this.responseBuilder(500, {
+                        sucessfull: false,
+                        reason: 'The selected parent document is a file, not a folder'
+                    });
+                }
+            } catch (e) {
+                // Folder does not exist
+                return this.responseBuilder(500, {
+                    sucessfull: false,
+                    reason: 'The selected folder does not exist.'
+                });
+            }
+        }
+
+
+
+        // Check document name
+        if (input.body.documentName.includes('/')) {
+            return this.responseBuilder(500, {
+                sucessfull: false,
+                reason: 'The folder contains invalid characters'
+            });
+        }
+
+        query.name += `/${input.body.documentName}`;
+
+        // Check if the document exists
+        try {
+            await mapper.get<Document>(query);
+            return this.responseBuilder(500, {
+                sucessfull: false,
+                reason: 'The file already exists'
+            });
+        } catch (e) {
+            // File does not exist
+        }
+
+        query.type = input.body.type;
+        query.created = new Date(input.body.date);
+        query.parent = input.body.parentFolder;
+
+        if (!!parentFolder) {
+            if (!!parentFolder.children) {
+                parentFolder.children.push(query.name);
+            } else {
+                parentFolder.children = [query.name];
+            }
+        }
+
+        try {
+            await mapper.put(query);
+        } catch (e) {
+            return this.responseBuilder(500, {
+                sucessfull: false,
+                reason: JSON.stringify(e)
+            });
+        }
+
+        return this.responseBuilder(200, {
+            sucessfull: true
+        });
     }
-};
+}
+
+export const create = toLambda(new CreateDocument());
