6d2496ee7a0986bbbc82830ccc253c99df4ba2e6
becelot <benedict.becker93@gmail.com>
2019-09-14 16:19:56 +0200

    Implemented serverless offline webpack watching service for local testing

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const slsw = require('serverless-webpack');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+const isLocal = slsw.lib.webpack.isLocal;
+
+module.exports = {
+    mode: isLocal ? 'development' : 'production',
+    entry: slsw.lib.entries,
+    externals: [nodeExternals()],
+    resolve: {
+        extensions: [ '.js', '.jsx', '.json', '.ts', '.tsx' ]
+    },
+    output: {
+        libraryTarget: 'commonjs',
+        path: path.join(__dirname, '.webpack'),
+        filename: '[name].js'
+    },
+    target: 'node',
+    module: {
+        rules: [
+            {
+                // Include ts, tsx, js, and jsx files.
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'ts-loader', options: {transpileOnly: true}}
+                ]
+            }
+        ]
+    },
+    plugins: [new ForkTsCheckerWebpackPlugin()]
+};
