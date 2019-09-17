7bdedf98125fd442edc328968c9786a2d953c0f8
becelot <benedict.becker93@gmail.com>
2019-09-17 16:23:45 +0200

    Added ListDocuments endpoint

--- src/functions/list.ts
+++ src/functions/list.ts
@@ -1,21 +1,71 @@
-import {AwsLambda, RestApiEvent, RestApiOutput} from "../utils/lambda";
+import {AwsLambda, RestApiEvent, RestApiOutput, toLambda} from "../utils/lambda";
 import {DocType} from "../models/document.type";
+import {Document} from "../models/document";
+import {Mapper} from "../utils/mapper";
 
 interface IListDocuments {
     folder: string;
 }
 
 interface IListDocumentsResp {
+    successful: boolean;
     documents: Array<{
         name: string;
         type: DocType;
     }>;
+    error?: string;
 }
 
 class ListDocuments extends AwsLambda<IListDocuments, IListDocumentsResp> {
     async perform(input: RestApiEvent<IListDocuments>): Promise<RestApiOutput<IListDocumentsResp>> {
+
+        if (!!input.body.folder) {
+            return this.responseBuilder(500, {
+                successful: false,
+                documents: [],
+                error: 'Folder does not exist'}
+                );
+        }
+
+        const userName = this.CurrentUserName;
+
+        const query: Document = new Document();
+        query.name = userName + '/' + input.body.folder;
+
+        const parent: Document = await Mapper.Instance.get(query);
+
+        if (parent.type === DocType.DOC_FOLDER) {
+            if (!parent.children) {
+                parent.children = [];
+            }
+
+            const children: string[] = [];
+            parent.children.forEach(child => children.push(parent.name + '/' + child));
+
+            const docs: Array<{
+                name: string;
+                type: DocType;
+            }> = [];
+
+            for (let child in children) {
+                const res = await Mapper.Instance.get(Object.assign(new Document(), child));
+                docs.push({
+                    name: res.name,
+                    type: res.type
+                });
+            }
+
+            return this.responseBuilder(200, {
+                        successful: true,
+                        documents: docs
+                    });
+        }
+
         return this.responseBuilder(200, {
+            successful: true,
             documents: []
         });
     }
 }
+
+export const list = toLambda(new ListDocuments());
