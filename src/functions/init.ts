997c8c3455b01a29133deed17cba30b305b64db9
becelot <benedict.becker93@gmail.com>
2019-09-18 15:56:04 +0200

    Added additional folder to init

--- src/functions/init.ts
+++ src/functions/init.ts
@@ -2,11 +2,13 @@ import {AwsLambda, RestApiOutput, toLambda} from "../utils/lambda";
 import {APIGatewayProxyEvent} from "aws-lambda";
 import {Mapper} from "../utils/mapper";
 import {Document} from "../models/document";
+import {DocType} from "../models/document.type";
 
 
 class InitDatabase extends AwsLambda<{}, {}> {
     async perform(input: Omit<APIGatewayProxyEvent, "body"> & { body: {} }): Promise<RestApiOutput<{}>> {
-        await Mapper.Instance.put(Object.assign(new Document(), { name: 'Becelot'}))
+        await Mapper.Instance.put(Object.assign(new Document(), { name: 'Becelot', type: DocType.DOC_FOLDER, children: ['Angular']}));
+        await Mapper.Instance.put(Object.assign(new Document(), { name: 'Becelot/Angular', type: DocType.DOC_FOLDER, parent: 'Becelot'}));
         return this.responseBuilder(200, {});
     }
 }
