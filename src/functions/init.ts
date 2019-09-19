15be517d4e9d9b09ba17eb66ab0e35559cc42ac7
becelot <benedict.becker93@gmail.com>
2019-09-19 14:57:10 +0200

    Added login test route

--- src/functions/init.ts
+++ src/functions/init.ts
@@ -8,7 +8,8 @@ import {DocType} from "../models/document.type";
 class InitDatabase extends AwsLambda<{}, {}> {
     async perform(input: Omit<APIGatewayProxyEvent, "body"> & { body: {} }): Promise<RestApiOutput<{}>> {
         await Mapper.Instance.put(Object.assign(new Document(), { name: 'Becelot', type: DocType.DOC_FOLDER, children: ['Angular']}));
-        await Mapper.Instance.put(Object.assign(new Document(), { name: 'Becelot/Angular', type: DocType.DOC_FOLDER, parent: 'Becelot'}));
+        await Mapper.Instance.put(Object.assign(new Document(), { name: 'Becelot/Angular', type: DocType.DOC_FOLDER, parent: 'Becelot', children: ['Login']}));
+        await Mapper.Instance.put(Object.assign(new Document(), { name: 'Becelot/Angular/Login', type: DocType.DOC_FOLDER, parent: 'Becelot/Angular'}));
         return this.responseBuilder(200, {});
     }
 }
