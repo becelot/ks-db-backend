b51d1f4dd6f6f57d6b79dfdf9bfe973e2da0ca99
becelot <benedict.becker93@gmail.com>
2019-09-21 15:14:41 +0200

    Implemented update logic

--- src/functions/update_document.ts
+++ src/functions/update_document.ts
@@ -1,4 +1,7 @@
 import {AwsLambda, RestApiEvent, RestApiOutput, toLambda} from "../utils/lambda";
+import {Document} from "../models/document";
+import {Mapper} from "../utils/mapper";
+import {DocType} from "../models/document.type";
 
 interface IUpdateDocument {
     path: string;
@@ -13,9 +16,33 @@ interface IUpdateDocumentResponse {
 
 class UpdateDocument extends AwsLambda<IUpdateDocument, IUpdateDocumentResponse> {
     async perform(input: RestApiEvent<IUpdateDocument>): Promise<RestApiOutput<IUpdateDocumentResponse>> {
+        if (input.body.path === undefined) {
+            return this.responseBuilder(500, {
+                successful: false,
+                error: 'Document path was not provided'}
+            );
+        }
+
+        // Get document
+        const userName = this.CurrentUserName;
+        const query: Document = new Document();
+        query.name = userName + (input.body.path === '' ? '' : '/' + input.body.path);
+
+        const document: Document = await Mapper.Instance.get(query);
+
+        if (document.type !== DocType.DOC_FILE) {
+            return this.responseBuilder(500, {
+                successful: false,
+                error: 'Document was not a text file'}
+            );
+        }
+
+        document.content = input.body.content;
+
+        await Mapper.Instance.update(document);
+
         return this.responseBuilder(200, {
-            successful: false,
-            error: 'Not implemented yet'
+            successful: true
         });
     }
 }
