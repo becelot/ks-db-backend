f3dc78fc918d8b91f2a83064c98b7ce62429b5e0
becelot <benedict.becker93@gmail.com>
2019-11-10 14:55:21 +0100

    removed unused import in document model

--- src/models/document.ts
+++ src/models/document.ts
@@ -1,7 +1,5 @@
 import {attribute, hashKey, rangeKey, table} from "@aws/dynamodb-data-mapper-annotations";
 import {DocType} from "./document.type";
-import {StringType} from "@aws/dynamodb-data-marshaller/build/SchemaType";
-import {embed} from "@aws/dynamodb-data-mapper";
 
 @table('ks-db-documents')
 export class Document {
